b93f8550063f33b087f139e43ec1926fbddfb34b
Dafa25 <dafarofi25@gmail.com>
2023-11-09 00:14:46 +0700

    soalno3

---
+++ detail_produk.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Produk</title>
+
+    <!-- Tambahkan link stylesheet Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-5">
+        <?php
+        if (isset($_GET['id'])) {
+            $product_id = $_GET['id'];
+        } else {
+            header("Location: error.php");
+            exit;
+        }
+
+        $host = "localhost";
+        $dbuser = "postgres";
+        $dbpass = "postgres";
+        $port = "5432";
+        $dbname = "postgres";
+        $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpass);
+
+        $query = "SELECT * FROM product WHERE id = :id";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':id', $product_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<h2>" . $row['name_product'] . "</h2>";
+            echo "<p>Price: Rp" . $row['price'] . "</p>";
+            echo "<p>Description: " . $row['description'] . "</p>";
+        }
+        ?>
+    </div>
+
+    <div class="container mt-5">
+        <?php
+        $query = "SELECT * FROM review WHERE product_id = :id";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':id', $product_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        echo "<h3>Ulasan Pembeli:</h3>";
+        while ($review = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<div class='card'>";
+            echo "<div class='card-body'>";
+            echo "<p class='card-text'>" . $review['review'] . "</p>";
+            echo "</div>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+
+    <!-- Tambahkan skrip JavaScript Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
